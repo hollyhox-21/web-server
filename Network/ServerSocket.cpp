2a73399061923781254570126faafa6675e9b4d2
zera <svetik.avtonomov@gmail.com>
2021-06-24 12:51:15 +0300

    Maded ServerSocket

---
+++ Network/ServerSocket.cpp
@@ -0,0 +1,38 @@
+#include "ServerSocket.hpp"
+
+ServerSocket::ServerSocket(int domain, int type, int protocol, int port, u_long interface, int backlog)
+	throw() : SocketBase(domain, type, protocol, port, interface) {
+	_backlog = backlog;
+
+	try {
+		std::cout << "Connecting" << std::endl;
+		connect();
+	}
+	catch (std::exception const & e) {
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		std::cout << "Listening" << std::endl;
+		startListening();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+}
+
+ServerSocket::~ServerSocket() { }
+
+void ServerSocket::connect() {
+	sockaddr_in address = getAddress();
+	if (bind(getSocket(), (struct sockaddr*)&address, sizeof(address)))
+		throw ConnectionException(strerror(errno));
+}
+
+void ServerSocket::startListening() {
+	if (listen(getSocket(), _backlog))
+		throw ListenException(strerror(errno));
+
+}
---
+++ Network/ServerSocket.hpp
@@ -0,0 +1,18 @@
+#ifndef SERVERSOCKET_HPP
+# define SERVERSOCKET_HPP
+
+#include "SocketBase.hpp"
+
+class ServerSocket : public SocketBase {
+	private:
+		int		_backlog;
+	public:
+		ServerSocket(int domain, int type, int protocol, int port, u_long interface, int _backlog) throw();
+		ServerSocket(ServerSocket const & s);
+		~ServerSocket();
+
+		void connect();
+		void startListening();
+};
+
+#endif
