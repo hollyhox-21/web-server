ed2a888d7eac8ce8b20145309735fd9695e30c51
lastsign <blackwishes123@gmail.com>
2021-06-26 20:27:28 +0300

    паршу локейшены

--- ParserConfigure.cpp
+++ ParserConfigure.cpp
@@ -5,9 +5,35 @@
 #include <string>
 #include <algorithm>
 
+std::string pars_one_arg(std::string &line)
+{
+	line.erase(0, line.find(":") + 1);
+	line.erase(0, line.find_first_not_of(" \t"));
+	int i;
+	if ((i = line.find(" ")) != -1)
+	{
+		std::string between = line;
+		between.erase(0, i);
+		between.erase(0, between.find_first_not_of(" "));		
+		if (!between.empty())
+		{
+			std::cout << "Error: wrong argument in config" << std::endl;
+			return "";
+		}
+		else
+			line.erase(i, line.length());
+	}
+}
+
 int main(int ac, char **av)
 {
 	std::vector<Server> serv = std::vector<Server>();
+	Location loc;
+	std::string locPath;
+	loc.index = "";
+	loc.pathCgi = "";
+	loc.root = "";
+	std::map<std::string, Location> location;
 	if (ac != 2)
 		return 0;
 	std::ifstream file(av[1]);
@@ -24,7 +50,10 @@ int main(int ac, char **av)
 		if (line == "server:")
 		{
 			// if (sflg == 1)
-				// serv.push_back(s);
+			// {
+			// 	s.setLocations(location);
+			// 	serv.push_back(s);
+			// }
 			// s = Server();
 			sflg = 1;
 			lflg = 0;
@@ -36,38 +65,72 @@ int main(int ac, char **av)
 			line.erase(0, 1);
 			if (line.find("server_name:") != std::string::npos)
 			{
-				line.erase(0, line.find(":") + 1);
-				line.erase(0, line.find_first_not_of(" \t"));
-				std::cout << line << std::endl;
-				int i;
-				if ((i = line.find(" ")) != -1)
-				{
-					std::string between = line;
-					between.erase(0, i);
-					std::cout << between << std::endl;
-					between.erase(0, between.find_first_not_of(" "));		
-					std::cout << between << std::endl;
-					if (!between.empty())
-					{
-						std::cout << "Error: wrong argument in config" << std::endl;
-						return -1;
-					}
-					else
-						line.erase(i, line.length());
-					std::cout << line << std::endl;
-				}
+				line = pars_one_arg(line);
 				std::cout << line << std::endl;
 				// s.setName(line);
 			}
 			else if (line.find("host:") != std::string::npos)
 			{
-				line.erase(0, line.rfind(" ") + 1);				
+				line = pars_one_arg(line);
+				std::cout << line << std::endl;
 				// s.setHost(line);
+			}
+			else if (line.find("port:") != std::string::npos)
+			{
+				line = pars_one_arg(line);
+				std::cout << line << std::endl;
+				// s.setPort(atoi(const_cast<char*>(line.c_str())));
+			}
+			else if (line.find("location:") != std::string::npos)
+			{
+				lflg = 1;
+				line = pars_one_arg(line);
+				std::cout << line << std::endl;
+				locPath = line;
 			}
 		}
 		else if (sflg == 1 && lflg == 1 && line[0] == '\t' && line[1] == '\t')
 		{
-
+			if (line.find("root:") != std::string::npos)
+			{
+				line = pars_one_arg(line);
+				std::cout << line << std::endl;
+				loc.root = line;
+				// s.setHost(line);
+			}
+			else if (line.find("path_cgi:") != std::string::npos)
+			{
+				line = pars_one_arg(line);
+				std::cout << line << std::endl;
+				loc.pathCgi = line;
+				// s.setHost(line);
+			}
+			else if (line.find("index:") != std::string::npos)
+			{
+				line = pars_one_arg(line);
+				std::cout << line << std::endl;
+				loc.index = line;
+				// s.setHost(line);
+			}
+			else if (line.find("allow_methods:") != std::string::npos)
+			{
+				std::map<std::string, bool> methods = std::map<std::string, bool>();
+				if (line.find("GET") != std::string::npos && loc.index != "")
+					methods.insert(std::make_pair<std::string, bool>("GET", 1));
+				else if (line.find("GET") != std::string::npos && loc.index == "")
+					return -1;
+				if (line.find("POST") != std::string::npos)
+					methods.insert(std::make_pair<std::string, bool>("POST", 1));
+				if (line.find("DELETE") != std::string::npos)
+					methods.insert(std::make_pair<std::string, bool>("DELETE", 1));
+				std::cout << line << std::endl;
+				loc.methods = methods;
+			}
+		}
+		else if (line == "" && lflg == 1)
+		{
+			lflg = 0;
+			location.insert(std::make_pair<std::string, Location>(locPath, loc));
 		}
 	}
 	return 0;
