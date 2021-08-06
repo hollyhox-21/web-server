9139cf88d110f12025f3f7fdcb6355fbb8d36402
Zera57 <svetik.avtonomov@gmail.com>
2021-08-06 12:01:23 +0300

    Write to console only html response

--- Network/ClientServer/Client.cpp
+++ Network/ClientServer/Client.cpp
@@ -94,6 +94,8 @@ bool	Client::getStage() {
 
 void	Client::setResponse(std::map<int, std::string> & errPage, std::map<std::string, Location> & locations) {
 	_res = new Response(_req, errPage, locations);
-	std::cout << "\n\nResponse\n";
-	write(1, _res->toFront().first, _res->toFront().second);
+	if (std::string(_res->toFront().first).find("text/html") != std::string::npos) {
+		std::cout << "\n\nResponse\n";
+		write(1, _res->toFront().first, _res->toFront().second);
+	}
 }
