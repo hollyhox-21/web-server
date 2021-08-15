b6b8252cca9ce349463370ae53e8f07f9a27beb8
lastsign <blackwishes123@gmail.com>
2021-08-15 13:43:41 +0300

    change location for cgi

--- Network/Models/Location.hpp
+++ Network/Models/Location.hpp
@@ -9,20 +9,21 @@ struct Location
 	std::string					path;
 	std::map<std::string, bool>	methods;
 	std::string					root;
-	std::string					pathCgi;
+	std::pair<std::string, std::string> cgi;
 	std::string					index;
 	std::string					autoindex;
 	int							limit_client_body_size;
 	void reset(){
 		methods = std::map<std::string, bool>();
 		root = std::string();
-		pathCgi = std::string();
+		cgi = std::pair<std::string, std::string>();
 		index = std::string();
 		autoindex = std::string();
 	}
 	
 	std::string getRoot() { return root; }
-	std::string getPathCGI() { return pathCgi; }
+
+	const std::pair<std::string, std::string> &getCgi() { return cgi; }
 	std::string getIndex() { return index; }
 	std::string getAutoIndex() { return autoindex; }
 	int			getLimit() { return limit_client_body_size; }
