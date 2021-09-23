bf13f8e08f512bed3423c47425291de9d40576e9
zera <svetik.avtonomov@gmail.com>
2021-09-23 23:05:15 +0300

    Changed logic but need fixes for body then send to parsing

--- Network/ClientServer/Client.cpp
+++ Network/ClientServer/Client.cpp
@@ -17,10 +17,13 @@ void	Client::recvChunked() {
 	std::cout << "Chunked" << std::endl;
 	while (_chunkedBody.find("\r\n") != std::string::npos) {
 		std::size_t index = _chunkedBody.find("\r\n") + 2;
-		std::string size = _chunkedBody.substr(index);
+		std::string size = _chunkedBody.substr(0, index);
 		_chunkedBody.erase(0, index);
+		// std::cout << "Size: " << size << std::endl;
 		contentLength = strtol(size.c_str(), & p, 16) + 2;
-		_body.append(_chunkedBody.substr(contentLength));
+		// std::cout << "Content-Length: " << contentLength << std::endl;
+		_body.append(_chunkedBody.substr(0, contentLength));
+		// std::cout << "Body: " << _body << std::endl;
 		index = _chunkedBody.find("\r\n") + 2;
 		_chunkedBody.erase(0, index);
 	}
@@ -28,8 +31,8 @@ void	Client::recvChunked() {
 
 
 Client::STATE	Client::recvHeaders() {
-	int nDataLength;
-	const char	*konec;
+	int			nDataLength;
+	std::size_t	konec;
 
 	std::cout << "------HEADERS-------\n";
 
@@ -41,9 +44,9 @@ Client::STATE	Client::recvHeaders() {
 	_buffer[nDataLength] = 0;
 	std::cout << "Сырой буфер\n" << _buffer << "--------------------\n";
 	_header.append(_buffer, nDataLength);
-	if ((konec = strstr(_header.c_str(), (const char*)"\r\n\r\n")) != NULL) {
+	if ((konec = _header.find("\r\n\r\n")) != std::string::npos) {
 		std::cout << "Сырые хедеры\n" << _header << "--------------------\n";
-		_body = _header.substr(konec + 4 - _header.c_str());
+		_body = _header.substr(konec + 4);
 		_chunkedBody = _body;
 		std::cout << _body;
 		_req.parsRequest(_header);
@@ -55,12 +58,12 @@ Client::STATE	Client::recvHeaders() {
 Client::STATE	Client::recvBody() {
 	int nDataLength;
 
-	std::cout << "------BODY-------\n";
 	if (_req.getMethod() != "GET" && _req.getMethod() != "HEAD") {
+		std::cout << "------BODY-------\n";
 		// if (_req.getValueMapHeader("Content-Length") == "")
 		// 	return -1;
-		if ((_req.getValueMapHeader("Transfer-Encoding") == "chunked" && _chunkedBody.find("0\r\n\r\n") != std::string::npos) || 
-					(int)_body.length() < atoi(_req.getValueMapHeader("Content-Length").c_str())){
+		if ((_req.getValueMapHeader("Transfer-Encoding") == "chunked")
+										 || (int)_body.length() < atoi(_req.getValueMapHeader("Content-Length").c_str())) {
 			nDataLength = recv(getSocket(), _buffer, BUFFER_SIZE, 0);
 			if (nDataLength == 0)
 				return (_state = END);
@@ -69,16 +72,21 @@ Client::STATE	Client::recvBody() {
 			_buffer[nDataLength] = 0;
 			std::cout << "Ya chitau " << nDataLength << "\n";
 			std::cout << _buffer;
-			std::cout << "--------------------\nBody: ";
+			std::cout << "--------------------\n";
 			if (_req.getValueMapHeader("Transfer-Encoding") == "chunked")
 				_chunkedBody.append(_buffer);
 			else
 				_body.append(_buffer);
-			std::cout << _chunkedBody << "Len: " << _chunkedBody.length() << "\n-----------------\n";
-		} else {
+			std::cout << "Body: \""<< _chunkedBody << "\"\n";
+			if (std::string::npos != _chunkedBody.find("0\r\n\r\n"))
+				std::cout << "find\n";
+			std::cout << "Len: " << _chunkedBody.length() << "\n-----------------\n";
+		} if (_chunkedBody.find("0\r\n\r\n") != std::string::npos) {
 			_state = END;
 			if (_req.getValueMapHeader("Transfer-Encoding") == "chunked")
 				recvChunked();
+			// std::cout << "Body: " << _body << std::endl;
+			return (_state);
 		}
 	} else {
 		return (_state = END);
@@ -95,6 +103,7 @@ Client::STATE		Client::recvMsg() {
 		if (recvBody() == BODY)
 			return (BODY);
 	}
+	_req.parsBody(_body);
 	std::cout << "konec";
 	return END;
 }
